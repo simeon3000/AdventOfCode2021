be5a8451d54b03f099ff621dd21224666671c0db
Simeon Georgiev <simeon.georgiev@atos.net>
2022-12-02 14:39:43 +0200

    2022 day01

---
+++ AdventOfCode2022/Day1.cs
@@ -0,0 +1,49 @@
+﻿namespace AdventOfCode2022;
+
+public class Day1 : IDay
+{
+    private const string file = @"inputs\day01.txt";
+    private static readonly List<string> input = Helper.GetInputLines(file);
+
+    public long Run1()
+    {
+        int maxSum = 0, currentSum = 0;
+        foreach (string item in input)
+        {
+            if (string.IsNullOrEmpty(item))
+            {
+                if (currentSum > maxSum)
+                {
+                    maxSum = currentSum;
+                }
+
+                currentSum = 0;
+                continue;
+            }
+
+            currentSum += int.Parse(item);
+        }
+
+        return maxSum;
+    }
+
+    public long Run2()
+    {
+        List<int> elves = new();
+        int currentSum = 0;
+        foreach (string item in input)
+        {            
+            if (!string.IsNullOrEmpty(item))
+            {
+                currentSum += int.Parse(item);                                               
+                continue;
+            }
+
+            elves.Add(currentSum);
+            currentSum = 0;
+        }
+
+        elves.Add(currentSum);
+        return elves.OrderByDescending(x => x).Take(3).Sum();
+    }
+}
---
+++ AdventOfCode2022/Program.cs
@@ -0,0 +1,10 @@
+﻿global using Shared;
+
+using AdventOfCode2022;
+
+IDay day = new Day1();
+
+Helper.PrintResult(day, "Run1", day.Run1());
+Helper.PrintResult(day, "Run2", day.Run2());
+
+Console.ReadKey();
