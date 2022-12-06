0fb1dcc2629d7cf3063bd22e42317dedb979121d
Simeon Georgiev <simeon.georgiev@atos.net>
2022-12-06 13:45:27 +0200

    day06 - GeneratedRegex

--- AdventOfCode2022/Day06.cs
+++ AdventOfCode2022/Day06.cs
@@ -2,29 +2,30 @@
 
 namespace AdventOfCode2022;
 
-public class Day06 : IDay
+public partial class Day06 : IDay
 {
     private const string file = @"inputs\day06.txt";
     private static readonly string input = Helper.GetInputLines(file).First();
 
-    private const string pattern = @"^(?!.*(.).*\1)[a-z]+$";
+    [GeneratedRegex("^(?!.*(.).*\\1)[a-z]+$")]
+    private static partial Regex NonRepeatableChar();
 
     public long Run1() => GetIndex(4);
     
     public long Run2() => GetIndex(14);
 
-    private int GetIndex(int markerLength)
+    private static int GetIndex(int markerLength)
     {
         for (int i = 0; i <= input.Length - markerLength; i++)
         {
             int endIndex = i + markerLength;
             string marker = input[i..endIndex];
-            if (Regex.IsMatch(marker, pattern))
+            if (NonRepeatableChar().IsMatch(marker))
             {
                 return endIndex;
             }
         }
 
         return -1;
-    }
+    }    
 }
