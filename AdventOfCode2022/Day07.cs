48d4281c413a0f4d67f13643969d2da974686d5b
Simeon Georgiev <simeon.georgiev@atos.net>
2022-12-10 14:02:22 +0200

    day07

---
+++ AdventOfCode2022/Day07.cs
@@ -0,0 +1,100 @@
+﻿namespace AdventOfCode2022;
+
+public class Day07 : IDay
+{
+    private const string file = @"inputs\day07.txt";
+    private static readonly List<string> input = Helper.GetInputLines(file);
+
+    private readonly Dir root = new();
+    private int minSpace;
+
+    public long Run1()
+    {
+        FillDirs();
+        return Calc1(root, 0);
+    }
+
+    private int Calc1(Dir dir, int sum)
+    {
+        if (dir.Size <= 100000)
+        { 
+            sum += dir.Size; 
+        }
+
+        foreach (Dir child in dir.Dirs.Values)
+        {
+          sum = Calc1(child, sum);
+        }
+
+        return sum;
+    }
+
+    public long Run2() => Calc2();
+
+    private int Calc2()
+    {
+        minSpace = root.Size - 40000000;
+
+        List<int> dirs = AddBigDirs(root, new List<int>());
+        dirs.Sort();
+
+        return dirs.First();
+    }
+
+    private List<int> AddBigDirs(Dir dir, List<int> bigDirs)
+    {
+        if (dir.Size >= minSpace)
+        {
+            bigDirs.Add(dir.Size);
+        }
+
+        foreach (Dir child in dir.Dirs.Values)
+        {
+            bigDirs = AddBigDirs(child, bigDirs);
+        }
+
+        return bigDirs;
+    }
+
+    private void FillDirs()
+    {
+        Dir currentDir = root;
+        foreach (string line in input.Skip(1))
+        {
+            string[] l = line.Split();
+            if (l[0] == "$")
+            {
+                string command = l[1];
+                if (command == "cd")
+                {
+                    string dirName = l[2];
+                    if (dirName == "..")
+                    {
+                        currentDir = currentDir.PreviousDir;
+
+                    }
+                    else
+                    {
+                        currentDir = currentDir.Dirs[dirName];
+                    }
+                }
+            }
+            else if (l[0] == "dir")
+            {
+                currentDir.Dirs.Add(l[1], new Dir { PreviousDir = currentDir });
+            }
+            else
+            {
+                currentDir.FilesSize += int.Parse(l[0]);
+            }
+        }
+    }
+}
+
+public record Dir
+{    
+    public Dir PreviousDir { get; init; }
+    public int FilesSize { get; set; }
+    public Dictionary<string, Dir> Dirs { get; } = new();
+    public int Size => FilesSize + Dirs.Values.Sum(x => x.Size);
+}
--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -2,7 +2,7 @@
 
 using AdventOfCode2022;
 
-IDay day = new Day06();
+IDay day = new Day07();
 
 Helper.PrintResult(day, "Run1", day.Run1());
 Helper.PrintResult(day, "Run2", day.Run2());
