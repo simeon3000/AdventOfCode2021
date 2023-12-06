14f8c5b336baece4b52857b4e31dea4c6e9bc1ba
Simeon Georgiev <simeon.georgiev@atos.net>
2023-12-06 13:05:15 +0200

    day 04

---
+++ AdventOfCode2023/Day04.cs
@@ -0,0 +1,78 @@
+﻿namespace AdventOfCode2023;
+
+public class Day04 : IDay
+{
+    private const string file = @"inputs\day04.txt";
+    private static readonly List<string> input = Helper.GetInputLines(file);
+    private static readonly string[] separator = [":", "|"];
+
+    public long Run1()
+    {
+        int result = 0;
+        foreach (string line in input)
+        {
+            string[] tokens = line.Split(separator, StringSplitOptions.None);
+            int[] winners = tokens[1].Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[] myNumbers = tokens[2].Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            int cnt = 0;
+            foreach (int current in myNumbers)
+            {
+                if (winners.Contains(current))
+                {
+                    cnt++;
+                }
+            }
+
+            if (cnt > 0)
+            {
+                result += (int)Math.Pow(2, cnt - 1);
+            }
+        }
+
+        return result;
+    }
+
+    public long Run2()
+    {
+        Dictionary<int, Card> cards = [];
+
+        foreach (string line in input)
+        {
+            string[] tokens = line.Split(separator, StringSplitOptions.None);
+            int cardNumber = int.Parse(tokens[0].Split().Last());
+            int[] winners = tokens[1].Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[] myNumbers = tokens[2].Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            int cnt = 0;
+            foreach (int current in myNumbers)
+            {
+                if (winners.Contains(current))
+                {
+                    cnt++;
+                }
+            }
+
+            cards.Add(cardNumber, new Card(Wins: cnt));
+        }
+
+        foreach (var card in cards)
+        {
+            for (int i = 0; i < card.Value.Counts; i++)
+            {
+                for (int j = 1; j <= card.Value.Wins; j++)
+                {
+                    cards[card.Key + j].Counts++;
+                }
+            }
+        }
+
+        int result = cards.Values.Sum(x => x.Counts);
+        return result;
+    }
+}
+
+public record Card(int Wins)
+{
+    public int Counts { get; set; } = 1;
+}
--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -2,7 +2,7 @@
 
 using AdventOfCode2023;
 
-Day03 day = new();
+Day04 day = new();
 
 Helper.PrintResult(day, "Run1", day.Run1());
 Helper.PrintResult(day, "Run2", day.Run2());
